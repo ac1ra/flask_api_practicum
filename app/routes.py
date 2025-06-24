9f9507b40bf670ce608b5fe91c6cb0e2396d3450
ac1ra <glombinski@gmail.com>
2025-06-24 16:53:10 +0300

    fixed position with css

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,29 @@
-from flask import render_template, request,redirect,url_for
+from flask import render_template, request, redirect, url_for
 from app import app
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
-# @app.route("/submit",methods =["POST","GET"])
-# def submit():
-#     if request.method =="POST":
-#         name = request.form.get("name")
-#         email = request.form.get("email")
-#         return render_template("result.html",name=name,email=email)
-#     else:
-#         return redirect(url_for("form"))
+
+@app.route("/submit", methods=["POST", "GET"])
+def submit():
+    if request.method == "POST":
+        name = request.contact.get("name")
+        email = request.contact.get("email")
+        text = request.contact.get("text")
+
+        return render_template("success.html", name=name, email=email, text=text)
+    else:
+        return redirect(url_for("/contact"))
