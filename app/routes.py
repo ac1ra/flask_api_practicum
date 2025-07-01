33dbbd6c8ba3ef59b6b97140b01b286f716bf006
ac1ra <glombinski@gmail.com>
2025-07-01 17:44:14 +0300

    update files

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,28 @@
 from flask import render_template, request, redirect, url_for
 from app import app
+from datetime import datetime
+import locale
 
 
 @app.route("/")
 def form():
-    return render_template("home.html")
+    locale.setlocale(locale.LC_ALL, 'ru_RU')
+
+    current_time = datetime.now()
+    return render_template("home.html", current_time_html=current_time)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    team_members = [
+        {'name': 'Alice', 'role': 'Developer'},
+        {'name': 'Bob', 'role': 'Designer'},
+        {'name': 'Charlie', 'role': 'Project Manager'}
+    ]
+    return render_template("about.html", team_members_html=team_members)
+
+
+dict_storage = {'name': "Alex", 'email': "alex@al.com"}
 
 
 @app.route("/contact")
@@ -25,6 +38,6 @@ def submit():
         email = request.form.get("email")
         text = request.form.get("text")
 
-        return render_template("contact.html", name=name, email=email, text=text, message_check=message_check)
+        return render_template("page.html", name_html=name, email_html=email, text_html=text, message_check_html=message_check, dict_storage_html=dict_storage)
     else:
         return redirect(url_for("/contact"))
